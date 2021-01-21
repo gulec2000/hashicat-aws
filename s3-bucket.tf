dbe469779cb74475b79720df5ac561ac418415f5
Serdar <gulec2000@gmail.com>
2021-01-21 21:20:54 +0000

    edited s3-bucket file

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,5 +2,8 @@ module "s3-bucket" {
   source  = "app.terraform.io/SERDAR-training/s3-bucket/aws"
   version = "1.15.0"
   bucket_prefix = "serdargulec"
-  
+  bucket = "my-s3-bucket"
+  attach_elb_log_delivery_policy = false
+  attach_policy = false
+  acl = "private"  
 }
